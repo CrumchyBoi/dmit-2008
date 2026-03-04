9f846cac1396b02dde1f81ae16677972b2bcbd0d
CrumchyBoi <kevinmparadis@gmail.com>
2026-03-04 13:38:38 -0700

    added 3rd bullet point

--- pages/index.js
+++ pages/index.js
@@ -36,6 +36,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">
+            Testing to see if I can add a third bullet point
+          </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
